d978c7e12920713f87e780b0fccdeff1d5bd61ac
poteto <naonyan1135@gmail.com>
2024-07-29 16:02:37 +0900

    秘密鍵

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
 
-app.use(cookieParser()) //* ミドルウェアで全部のリクエスト
+app.use(cookieParser("mysecret")) //* ミドルウェアで全部のリクエスト 秘密鍵にmysecretを渡してる
 
-app.get("/greet",(req,res)=>{
-console.log(req.cookies) //* { name: 'yamada', animal: 'cat' }
-const {name="anonymous"} = req.cookies
+app.get("/greet", (req, res) => {
+  console.log(req.cookies) //* { name: 'yamada', animal: 'cat' }
+  const { name = "anonymous" } = req.cookies
   res.send(`やっほ- ${name}`)
 })
 
-app.get("/setname",(req,res)=>{
-  res.cookie("name","yamada") //* responseも返さないと
-  res.cookie("animal","cat") //* 複数かけるよ
+app.get("/setname", (req, res) => {
+  res.cookie("name", "yamada") //* responseも返さないと
+  res.cookie("animal", "cat") //* 複数かけるよ
   res.send("cookieをおくったよ")
 })
 
-app.listen(3000,()=>{
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true }) //* 署名付き
+  res.send("署名付きcookie返したよ") //* fruit: s%3Agrape.iAFRE8iCRHWXRV8ttPvGw91hOZJMYJ5lSGfOMSmBiYo
+  //* 署名付きcookieは隠すためじゃない
+})
+
+app.get("/verifyfruit", (req, res) => {
+  console.log(req.cookies)
+  // res.send(req.cookies)
+  // {
+  // "animal": "cat",
+  // "color": "perpul",
+  // "name": "manko"
+  // }
+  res.send(req.signedCookies)
+  // {
+  //   "fruit": "grape"
+  //   }
+})
+
+app.listen(3000, () => {
   console.log("実行したよ")
 })
