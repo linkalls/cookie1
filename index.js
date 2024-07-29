347045337f5f34440836034088dc69a31f103fd4
poteto <naonyan1135@gmail.com>
2024-07-29 15:42:59 +0900

    res.cookie(name,value)

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/greet",(req,res)=>{
   res.send("やっほ")
 })
 
+app.get("/setname",(req,res)=>{
+  res.cookie("name","yamada") //* responseも返さないと
+  res.send("cookieをおくったよ")
+})
+
 app.listen(3000,()=>{
   console.log("実行したよ")
 })
